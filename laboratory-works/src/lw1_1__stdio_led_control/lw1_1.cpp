e2a4f5fe6a0a54c3b438b06a3d596250047d07c8
Mihai Gurduza <gurduzamihai@gmail.com>
2026-02-11 20:15:00 +0200

    fix: simplify led setup

--- laboratory-works/src/lw1_1__stdio_led_control/lw1_1.cpp
+++ laboratory-works/src/lw1_1__stdio_led_control/lw1_1.cpp
@@ -3,20 +3,11 @@
 #include "dd_led.h"
 #include <Arduino.h>
 
-Led* led_1_1 = nullptr;
+Led led_1_1(LED_BUILTIN);
 
 void lab_1_1_setup() {
     srvSerialSetup();
     printf("Serial communication initialized!\n");
-    printf("Input the LED pin number (leave empty for built-in LED):\n");
-    int pin;
-    if (scanf("%d", &pin) == 1) {
-        led_1_1 = new Led(pin);
-        printf("Using LED on pin %d\n", pin);
-    } else {
-        led_1_1 = new Led();
-        printf("Using built-in LED on pin %d\n", LED_BUILTIN);
-    }
     printf("Available commands:\n  'ledon' - turn the LED on\n  'ledoff' - turn the LED off\n");
 }
 
@@ -25,17 +16,17 @@ void lab_1_1_loop() {
     if (scanf("%10s", c) > 0) {
         printf("Received command: %s\n", c);
         if (strcmp(c, "ledon") == 0) {
-            if (led_1_1->isOn()) {
+            if (led_1_1.isOn()) {
                 printf("Led is already on\n");
             } else {
-                led_1_1->on();
+                led_1_1.on();
                 printf("Led was Turned On\n");
             }
         } else if (strcmp(c, "ledoff") == 0) {
-            if (!led_1_1->isOn()) {
+            if (!led_1_1.isOn()) {
                 printf("Led is already off\n");
             } else {
-                led_1_1->off();
+                led_1_1.off();
                 printf("Led was Turned Off\n");
             }
         } else {
